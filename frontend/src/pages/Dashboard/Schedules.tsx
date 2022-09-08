e55f156c8b0697dd6d611e540e6f9e403906dfa1
Kevin Hu <kevinhuwest@gmail.com>
2022-09-08 10:56:10 -0700

    Empty state for schedules list view (#8)

--- frontend/src/pages/Dashboard/Schedules.tsx
+++ frontend/src/pages/Dashboard/Schedules.tsx
@@ -15,8 +15,48 @@ export const Schedules: React.FC = () => {
         })
     }, [])
 
+    let innerTable
+
+    if (schedules.length === 0) {
+        innerTable = (
+            <div className="mx-6 flex h-full flex-col items-center justify-center rounded-xl bg-gray-100">
+                <h1 className="text-2xl font-semibold">No schedules</h1>
+                <p className="mt-8 text-gray-600">
+                    Create a schedule to run a script at specific times
+                </p>
+            </div>
+        )
+    } else {
+        innerTable = (
+            <Table columnNames={['Name', 'Schedule', 'Updated']}>
+                {schedules.map((schedule) => {
+                    return (
+                        <tr>
+                            <td className="py-4">
+                                <Link
+                                    to={`/app/schedules/${schedule.id}`}
+                                    className="w-full py-4 pl-4 pr-8 hover:text-emerald-500"
+                                >
+                                    {schedule.name}
+                                </Link>
+                            </td>
+                            <td className="pl-4">
+                                {schedule.minute} {schedule.hour}{' '}
+                                {schedule.day_of_month} {schedule.month_of_year}{' '}
+                                {schedule.day_of_week}
+                            </td>
+                            <td className="pl-4">
+                                {getTimeSince(schedule.updated_at)}
+                            </td>
+                        </tr>
+                    )
+                })}
+            </Table>
+        )
+    }
+
     return (
-        <div className="flex flex-col space-y-6 px-8 py-16">
+        <div className="flex h-full flex-col space-y-6 px-8 py-16">
             <div className="flex items-center space-x-6 px-6">
                 <h1 className="text-2xl">Schedules</h1>{' '}
                 <Link
@@ -26,35 +66,7 @@ export const Schedules: React.FC = () => {
                     Create schedule <HiOutlineArrowRight className="ml-1" />
                 </Link>
             </div>
-            <div>
-                <div className="flex-col space-y-2">
-                    <Table columnNames={['Name', 'Schedule', 'Updated']}>
-                        {schedules.map((schedule) => {
-                            return (
-                                <tr>
-                                    <td className="py-4">
-                                        <Link
-                                            to={`/app/schedules/${schedule.id}`}
-                                            className="w-full py-4 pl-4 pr-8 hover:text-emerald-500"
-                                        >
-                                            {schedule.name}
-                                        </Link>
-                                    </td>
-                                    <td className="pl-4">
-                                        {schedule.minute} {schedule.hour}{' '}
-                                        {schedule.day_of_month}{' '}
-                                        {schedule.month_of_year}{' '}
-                                        {schedule.day_of_week}
-                                    </td>
-                                    <td className="pl-4">
-                                        {getTimeSince(schedule.updated_at)}
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                    </Table>
-                </div>
-            </div>
+            <div className="h-full flex-grow">{innerTable}</div>
         </div>
     )
 }
