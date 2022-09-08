2708b47ef28c246030b99dabb520e34e4c2eb492
Kevin Hu <kevinhuwest@gmail.com>
2022-09-08 16:01:00 -0700

    Add token type in CLI (#21)

--- cli/src/common/auth.ts
+++ cli/src/common/auth.ts
@@ -13,6 +13,15 @@ interface RefreshResponse {
     access_token: string
 }
 
+interface TokenType {
+    access_token: string
+    refresh_token: string
+    id_token: string
+    scope: string
+    expires_in: number
+    token_type: string
+}
+
 // see https://stackoverflow.com/questions/16316330/how-to-write-file-if-parent-folder-doesnt-exist
 export const writeFile = async (path: string, contents: string, callback) => {
     await fs.mkdir(dirname(path), { recursive: true }, function (err) {
@@ -25,7 +34,7 @@ export const writeFile = async (path: string, contents: string, callback) => {
 export const getTokens = async (
     { debug }: { debug: boolean } = { debug: false }
 ) => {
-    const tokens = getFile({ filePath: TOKENS_FILE, debug })
+    const tokens: TokenType = getFile({ filePath: TOKENS_FILE, debug })
 
     const access_token = tokens.access_token
 
