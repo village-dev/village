f4140a358b3898cb2cca339a5e6d1df7b3f72baa
Kevin Hu <kevinhuwest@gmail.com>
2022-09-08 18:13:54 -0700

    Search for scripts (#25)

--- frontend/src/pages/Dashboard/Scripts.tsx
+++ frontend/src/pages/Dashboard/Scripts.tsx
@@ -10,7 +10,10 @@ import { Script } from '../../../api/models/Script'
 
 export const Scripts: React.FC = () => {
     const { user } = useUserContext()
-    const [scripts, setScripts] = useState<Script[] | null>(null)
+    const [scripts, setScripts] = useState<Script[]>([])
+    const [query, setQuery] = useState('')
+    const [filteredScripts, setFilteredScripts] = useState<Script[]>([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         if (!user?.currentWorkspace) return
@@ -18,12 +21,24 @@ export const Scripts: React.FC = () => {
             .listScripts(user?.currentWorkspace.workspace_id)
             .then((s) => {
                 setScripts(s)
+                setLoading(false)
             })
     }, [user?.currentWorkspace])
 
+    useEffect(() => {
+        setFilteredScripts(
+            scripts.filter((script) => {
+                return (
+                    script?.name.toLowerCase().includes(query.toLowerCase()) ||
+                    script?.id.toLowerCase().includes(query.toLowerCase())
+                )
+            })
+        )
+    }, [query])
+
     let innerTable
 
-    if (scripts === null) {
+    if (loading) {
         innerTable = (
             <div className="mx-6 flex h-full flex-col items-center justify-center rounded-xl bg-gray-100">
                 <h1 className="text-2xl font-semibold text-gray-700">
@@ -44,29 +59,59 @@ export const Scripts: React.FC = () => {
             </div>
         )
     } else {
+        let searchResults
+
+        if (filteredScripts.length === 0) {
+            searchResults = (
+                <tr>
+                    <td colSpan={3} align="center" className="py-16 ">
+                        <h1 className="text-lg font-semibold text-gray-400">
+                            No scripts found
+                        </h1>
+                    </td>
+                </tr>
+            )
+        } else {
+            searchResults = scripts.map((script) => {
+                return (
+                    <tr>
+                        <td className="py-4">
+                            <Link
+                                to={`/app/scripts/${script.id}`}
+                                className="w-full py-4 pl-4 pr-8 hover:text-emerald-500"
+                            >
+                                {script.name}
+                            </Link>
+                        </td>
+                        <td className="pl-4">
+                            {script.engine} {script.engine_version}
+                        </td>
+                        <td className="pl-4">
+                            {getTimeSince(script.updated_at)}
+                        </td>
+                    </tr>
+                )
+            })
+        }
+
         innerTable = (
-            <Table columnNames={['Name', 'Engine', 'Updated']}>
-                {scripts.map((script) => {
-                    return (
-                        <tr>
-                            <td className="py-4">
-                                <Link
-                                    to={`/app/scripts/${script.id}`}
-                                    className="w-full py-4 pl-4 pr-8 hover:text-emerald-500"
-                                >
-                                    {script.name}
-                                </Link>
-                            </td>
-                            <td className="pl-4">
-                                {script.engine} {script.engine_version}
-                            </td>
-                            <td className="pl-4">
-                                {getTimeSince(script.updated_at)}
-                            </td>
-                        </tr>
-                    )
-                })}
-            </Table>
+            <>
+                <div className="px-6">
+                    <input
+                        type="text"
+                        onChange={(e) => {
+                            setQuery(e.target.value)
+                        }}
+                        className=" mb-4 w-full rounded-lg border-2 border-gray-200 px-3 py-2 focus:border-gray-400 focus:outline-none"
+                        placeholder="Search"
+                    />
+                </div>
+                <div className="mx-6 mt-4">
+                    <Table columnNames={['Name', 'Engine', 'Updated']}>
+                        {searchResults}
+                    </Table>
+                </div>
+            </>
         )
     }
 
@@ -81,7 +126,7 @@ export const Scripts: React.FC = () => {
                     Create script <HiOutlineArrowRight className="ml-1" />
                 </Link>
             </div>
-            <div className="flex-grow">{innerTable}</div>
+            <div className="h-full flex-grow">{innerTable}</div>
         </div>
     )
 }
