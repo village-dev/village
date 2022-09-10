593cdba7070f1046995350f93a0d87c55bdc29f1
Kevin Hu <kevinhuwest@gmail.com>
2022-09-09 22:40:05 -0700

    Init table for script schedules (#43)

--- frontend/src/pages/Dashboard/Script.tsx
+++ frontend/src/pages/Dashboard/Script.tsx
@@ -8,6 +8,9 @@ import { RunScriptEmbeddable } from './RunScript'
 import { Tab } from '@headlessui/react'
 import { RiExternalLinkLine } from 'react-icons/ri'
 import { BeatLoader } from 'react-spinners'
+import { Table } from '@components/Table'
+import { ListDropdown } from '@components/ListDropdown'
+import { MdDeleteOutline } from 'react-icons/md'
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -87,45 +90,87 @@ export const Runs: React.FC<{ runs: Run[] }> = ({ runs }) => {
     )
 }
 
+const NoSchedules: React.FC = () => {
+    return (
+        <div className="mx-6 flex h-full flex-col items-center justify-center rounded-xl bg-gray-100">
+            <h1 className="text-2xl font-semibold">No schedules</h1>
+            <p className="mt-8 text-gray-600">
+                Create a schedule to run a script at specific times
+            </p>
+        </div>
+    )
+}
+
+const NoResults: React.FC = () => {
+    return (
+        <h1 className="text-lg font-semibold text-gray-400">
+            No schedules found
+        </h1>
+    )
+}
+
+const searchFilter = ({ query, data }: { query: string; data: Schedule }) => {
+    const schedule = data
+    const script = schedule.script
+
+    return (
+        script?.name.toLowerCase().includes(query.toLowerCase()) ||
+        script?.id.toLowerCase().includes(query.toLowerCase()) ||
+        schedule.name.toLowerCase().includes(query.toLowerCase())
+    )
+}
+
+const ScheduleRow: React.FC<{ data: Schedule }> = ({ data }) => {
+    const schedule = data
+
+    return (
+        <tr>
+            <td className="py-4">
+                <Link
+                    to={`/app/schedules/${schedule.id}`}
+                    className="w-full py-4 pl-4 pr-8 hover:text-emerald-500"
+                >
+                    {schedule.name}
+                </Link>
+            </td>
+            <td className="pl-4">
+                {schedule.minute} {schedule.hour} {schedule.day_of_month}{' '}
+                {schedule.month_of_year} {schedule.day_of_week}
+            </td>
+            <td className="pl-4">{getTimeSince(schedule.updated_at)}</td>
+            <td>
+                <ListDropdown
+                    options={[
+                        {
+                            id: 'delete',
+                            name: 'Delete',
+                            icon: <MdDeleteOutline className="mr-2 text-xl" />,
+                            handler: () => {
+                                // TODO: Delete schedule
+                                console.log('Delete')
+                            },
+                        },
+                    ]}
+                />
+            </td>
+        </tr>
+    )
+}
+
 export const Schedules: React.FC<{ schedules: Schedule[] }> = ({
     schedules,
 }) => {
     return (
         <div className="flex-col space-y-2">
-            <div className="my-4 flex px-6 font-semibold">
-                <div className="w-64">
-                    <h2>Schedule ID</h2>
-                </div>
-                <div className="w-96">
-                    <h2>Schedule</h2>
-                </div>
-                <div className="w-32">
-                    <h2>Time</h2>
-                </div>
-            </div>
-            {schedules.map((schedule) => {
-                return (
-                    <Link
-                        key={schedule.id}
-                        to={`/build/${schedule.id}`}
-                        className="flex border-b-2 border-transparent px-6 hover:border-slate-200 hover:bg-slate-50"
-                    >
-                        <div className="w-64">
-                            <p>{schedule.id}</p>
-                        </div>
-                        <div className="w-96">
-                            <p>
-                                {schedule.minute} {schedule.hour}{' '}
-                                {schedule.day_of_month} {schedule.month_of_year}{' '}
-                                {schedule.day_of_week}
-                            </p>
-                        </div>
-                        <div className="w-32">
-                            <p>{getTimeSince(schedule.created_at)}</p>
-                        </div>
-                    </Link>
-                )
-            })}
+            <Table
+                loading={false}
+                emptyState={<NoSchedules />}
+                noResultsState={<NoResults />}
+                columnNames={['Name', 'Schedule', 'Updated', '']}
+                rowData={schedules}
+                RowRenderer={ScheduleRow}
+                searchFilter={searchFilter}
+            />
         </div>
     )
 }
