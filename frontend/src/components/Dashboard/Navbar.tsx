771b66a321793351073bbbd88f9208325f429a31
John Sungjin Kim <jsjkim00@gmail.com>
2022-09-10 00:07:41 -0700

    updated logo svg, worked on navbar

--- frontend/src/components/Dashboard/Navbar.tsx
+++ frontend/src/components/Dashboard/Navbar.tsx
@@ -3,7 +3,8 @@ import { useUserContext } from '@contexts/UserContext'
 import { Listbox, Transition } from '@headlessui/react'
 import { ChevronUpDownIcon } from '@heroicons/react/24/outline'
 import { NavLink } from 'react-router-dom'
-import { ReactComponent as LogoIcon } from '@components/logo.svg'
+// import { ReactComponent as LogoIcon } from '@components/logo.svg'
+import { ReactComponent as LogoIcon } from '../../../public/logo.svg'
 
 function WorkspaceListBox() {
     const { user, setCurrentWorkspace } = useUserContext()
@@ -16,14 +17,14 @@ function WorkspaceListBox() {
                 value={user?.currentWorkspace}
                 onChange={setCurrentWorkspace}
             >
-                <Listbox.Button className="relative w-full cursor-default truncate bg-white py-2 pl-4 pr-6 text-sm text-black">
+                <Listbox.Button className="relative w-full cursor-default truncate pl-4 pr-6 text-sm font-medium text-black">
                     <span className="block truncate">
                         {user?.currentWorkspace?.workspace?.name ||
                             'Loading...'}
                     </span>
                     <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                         <ChevronUpDownIcon
-                            className="h-5 w-5 text-black"
+                            className="h-5 w-5 bg-cream text-black"
                             aria-hidden="true"
                         />
                     </span>
@@ -71,10 +72,17 @@ function WorkspaceListBox() {
 
 export const Navbar: React.FC = () => {
     return (
-        <nav className="mb-16 h-12 w-screen border-b">
-            <LogoIcon />
-            Village
+        <nav className="mb-16 flex h-12 w-screen items-center space-x-4 border-b bg-cream pl-4 text-sm">
+            <span className="flex items-center space-x-2">
+                <LogoIcon className="h-5 w-5" />
+                <span className="font-display text-xl font-bold italic text-green">
+                    village
+                </span>
+            </span>
             <WorkspaceListBox />
+            <a href="http://docs.village.dev" className="text-gray-700">
+                Documentation
+            </a>
             {/* <NavLink
                     to="/app/new/workspace"
                     className="rounded-lg border p-2 text-xl text-zinc-400 shadow-md"
