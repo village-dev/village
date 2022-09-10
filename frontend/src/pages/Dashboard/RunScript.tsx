7be9dfdabece78db61767069582000a0af309955
Kevin Hu <kevinhuwest@gmail.com>
2022-09-09 22:41:24 -0700

    Loader for standalone script runs

--- frontend/src/pages/Dashboard/RunScript.tsx
+++ frontend/src/pages/Dashboard/RunScript.tsx
@@ -7,6 +7,7 @@ import { useEffect, useMemo, useState } from 'react'
 import Datetime from 'react-datetime'
 import 'react-datetime/css/react-datetime.css'
 import { useParams } from 'react-router-dom'
+import { BeatLoader } from 'react-spinners'
 import { Param, ParamType, ScriptWithMeta } from '../../../api'
 
 interface Option {
@@ -309,7 +310,13 @@ export const RunScriptStandalone = () => {
         })
     }, [])
 
-    if (!script) return null
+    if (script === null) {
+        return (
+            <div className="flex h-full w-full items-center justify-center">
+                <BeatLoader color="rgb(52 211 153)" />
+            </div>
+        )
+    }
 
     return (
         <div className="max-w-screen-md space-y-6 px-8 py-16">
