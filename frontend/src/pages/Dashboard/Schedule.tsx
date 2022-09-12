b7b7f69b08370a35695b9ce14ce92989504bb8bb
Kevin Hu <kevinhuwest@gmail.com>
2022-09-11 19:48:23 -0700

    Render runs table in schedules view (#70)
    * Render runs table in schedules view
    
    * Type fixes

--- frontend/src/pages/Dashboard/Schedule.tsx
+++ frontend/src/pages/Dashboard/Schedule.tsx
@@ -1,17 +1,73 @@
 import { getTimeSince } from '@common/dates'
 import { VillageClient } from '@common/VillageClient'
+import { NoRunResults } from '@components/EmptyStates/NoRunResults'
+import { NoRuns } from '@components/EmptyStates/NoRuns'
+import { Table } from '@components/Table'
 import { PageLoading } from '@pages/PageLoading'
 import cronParser from 'cron-parser'
 import cronstrue from 'cronstrue'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { ScheduleWithScriptAndRuns } from '../../../api'
+import { Link, useParams } from 'react-router-dom'
+import { Run, ScheduleWithMeta } from '../../../api'
+
+const RunRow: React.FC<{ data: Run }> = ({ data }) => {
+    const run = data
+
+    let trigger
+
+    if (run.schedule !== undefined) {
+        trigger = <span className="text-gray-500">Scheduled</span>
+    } else if (run.created_by !== undefined) {
+        trigger = <span className="text-gray-500">Manual</span>
+    }
+
+    return (
+        <tr key={run.id}>
+            <td className="py-4">
+                <Link
+                    to={`/app/runs/${run.id}`}
+                    className="w-full py-4 pl-4 pr-8 hover:text-emerald-500"
+                >
+                    {run.id}
+                </Link>
+            </td>
+            <td className="pl-4">{run.status}</td>
+            <td className="pl-4">{getTimeSince(run.created_at)}</td>
+            <td>{trigger}</td>
+        </tr>
+    )
+}
+
+const searchRunsFilter = ({ query, data }: { query: string; data: Run }) => {
+    const run = data
+    const script = run.build?.script
+
+    return (
+        script?.name.toLowerCase().includes(query.toLowerCase()) ||
+        script?.id.toLowerCase().includes(query.toLowerCase()) ||
+        run.status.toLowerCase().includes(query.toLowerCase())
+    )
+}
+
+export const Runs: React.FC<{ runs: Run[] }> = ({ runs }) => {
+    return (
+        <div className="flex-col space-y-2">
+            <Table
+                loading={false}
+                emptyState={<NoRuns />}
+                noResultsState={<NoRunResults />}
+                columnNames={['ID', 'Status', 'Updated', 'Trigger', '']}
+                rowData={runs}
+                RowRenderer={RunRow}
+                searchFilter={searchRunsFilter}
+            />
+        </div>
+    )
+}
 
 export const Schedule = () => {
     const { id } = useParams()
-    const [schedule, setSchedule] = useState<ScheduleWithScriptAndRuns | null>(
-        null
-    )
+    const [schedule, setSchedule] = useState<ScheduleWithMeta | null>(null)
 
     useEffect(() => {
         if (id === undefined) return
@@ -58,17 +114,9 @@ export const Schedule = () => {
                 </div>
                 <div>
                     <h1 className="text-xl">Runs</h1>
-                    {(schedule.runs ?? []).map((run) => (
-                        <div className="flex flex-row space-x-4">
-                            <div className="w-32">
-                                <p>{run.id}</p>
-                                <p>{run.status}</p>
-                            </div>
-                            <div className="w-32">
-                                <p>{getTimeSince(run.created_at)}</p>
-                            </div>
-                        </div>
-                    ))}
+                    <div className="mt-4 w-full rounded-xl border bg-white p-3">
+                        <Runs runs={schedule?.runs || []} />
+                    </div>
                 </div>
             </div>
         </div>
